00151ce3d32fb49b291723faacdc58e5855ac59e
Guiñazu Gaston <101716184+GastonGuinazu@users.noreply.github.com>
2022-10-29 16:46:18 -0300

    ApiFarmatown 1.2

---
+++ farmatownApi/API/Models/UsuarioModel.cs
@@ -0,0 +1,11 @@
+namespace API.Models;
+
+public class UsuarioModel
+{
+    //public int idUsuario { get; set; }
+    public string? usuario { get; set; }
+    public string? pwd { get; set; }
+   // public string? nombre { get; set; }
+   // public string? apellido { get; set; }
+   // public int? idTipoUsuario { get; set; }
+}
